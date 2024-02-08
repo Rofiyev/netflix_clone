5290fb56576272541d5dbdf1126284a81206c244
rof1yev <rofiyevdilshod@gmail.com>
2024-02-08 23:55:22 +0500

    change file

--- components/shared/login.tsx
+++ components/shared/login.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 import { Button } from "../ui/button";
 import { AiFillGithub } from "react-icons/ai";
 import { signIn } from "next-auth/react";
